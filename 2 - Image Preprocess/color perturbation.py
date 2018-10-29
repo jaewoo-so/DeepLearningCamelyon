bf64d82e7c62c7d6aa9667d5a6f82191953fc51a
Weizhe Li <weizheli@gmail.com>
2018-10-29 15:41:40 -0400

    Update color perturbation.py

--- 2 - Image Preprocess/color perturbation.py
+++ 2 - Image Preprocess/color perturbation.py
@@ -1,12 +1,14 @@
+#!/home/wli/env  python3
+# this is based on the description of liu yun's paper,"Detecting Cancer Metastases on Gigapixel Pathology Images".
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
+#use the random functions from tensorflow
 def color_perturbation(image):
       
       image = tf.image.random_brightness(image, max_delta=64./ 255.)
-      image = tf.image.random_saturation(image, lower=0, upper=0.25)
+      image = tf.image.random_saturation(image, lower=0.75, upper=1.25)
       image = tf.image.random_hue(image, max_delta=0.04)
-      image = tf.image.random_contrast(image, lower=0, upper=0.75)
+      image = tf.image.random_contrast(image, lower=0.25, upper=1.75)
       
       
       
@@ -14,10 +16,11 @@ def color_perturbation(image):
       return image
 
 
-image_color_perturb = color_perturbation(image)
-
+image_color_perturb = color_perturbation(m)
+#run tensorflow
 init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 im = sess.run(image_color_perturb)
-plt.imshow(npf)
+#show the result
+plt.imshow(im)
